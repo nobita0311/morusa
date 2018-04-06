0f15ae34bd57106af5194319b0a40b44aa9a4f9a
sekine <sekine@dnsk.jp>
2018-04-06 19:08:58 +0900

    テスト分けてみる

---
+++ tests/locale/enTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace nobita0311\MorusaTest\Locale;
+
+use nobita0311\Morusa\Locale\en;
+
+class enTest extends \PHPUnit_Framework_TestCase {
+
+    function __construct($name = null, array $data = array(), $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+    }
+
+    /**
+     * つかいかた
+     */
+    public function test_how_to_use_en() {
+        $morusa = new en();
+        $this->assertEquals($morusa->toMorseCode("A"), ".-");
+        $this->assertEquals($morusa->toMorseCode("B"), "-...");
+        $this->assertEquals($morusa->toMorseCode("AB"), ".- -...");
+        $this->assertEquals($morusa->toMorseCode("ab"), ".- -...");
+
+        $this->assertEquals($morusa->toMorseCode("123"), ".---- ..--- ...--");
+
+        $this->assertEquals($morusa->fromMorseCode(".-"), "A");
+        $this->assertEquals($morusa->fromMorseCode("-..."), "B");
+        $this->assertEquals($morusa->fromMorseCode(".- -..."), "AB");
+
+        $this->assertEquals($morusa->fromMorseCode(".---- ..--- ...--"), "123");
+    }
+
+}
